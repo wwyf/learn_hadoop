577a4269181b388b06f88e10f505def5428cfc11
walker <WalkerYF@yeah.net>
2018-06-27 09:48:55 +0800

    update

--- 1-word_count/complie.sh
+++ 1-word_count/complie.sh
@@ -2,4 +2,4 @@
 # 编译
 javac -classpath /opt/hadoop-1.2.1/hadoop-core-1.2.1.jar:/opt/hadoop-1.2.1/lib/commons-cli-1.2.jar -d work_count_class WordCount.java
 
-jar -cvf word_count_class/wordcount.jar work_count_class/*.class
+jar -cvf word_count_class/wordcount.jar word_count_class/*.class
