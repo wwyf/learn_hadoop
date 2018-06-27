70d4dbe4c5e91104b5b5f60daab7b3c5d162dbfc
walker <WalkerYF@yeah.net>
2018-06-27 10:16:00 +0800

    update

--- 1-word_count/complie.sh
+++ 1-word_count/complie.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # 编译
 javac -classpath /opt/hadoop-1.2.1/hadoop-core-1.2.1.jar:/opt/hadoop-1.2.1/lib/commons-cli-1.2.jar -d word_count_class WordCount.java
-
-jar -cvf word_count_class/wordcount.jar word_count_class/*.class
+# NOTICE:很奇怪，如果不在那个目录下jar的话会出问题，找不到类
+cd word_count_class
+jar -cvf wordcount.jar  *.class
